9f636d93108e93bf9ba104e9ed28b04bb1fe2090
CeMaGo <119850565+CeMaGo@users.noreply.github.com>
2023-09-24 20:19:28 +0200

    time to hunt some errors...

--- recap-project-3/recap-project-3_rick-and-morty-app/index.js
+++ recap-project-3/recap-project-3_rick-and-morty-app/index.js
@@ -1,17 +1,14 @@
 import createCard from "./components/card/card.js";
 import createNavButton from "./components/nav-button/nav-button.js";
-// import createPagination from "./components/nav-pagination/nav-pagination.js";
-// import createSearchBar from "./components/search-bar/search-bar.js";
+import createPagination from "./components/nav-pagination/nav-pagination.js";
+ import createSearchBar from "./components/search-bar/search-bar.js";
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
-const searchBar = document.querySelector('[data-js="search-bar"]');
+
 const navigation = document.querySelector('[data-js="navigation"]');
-// const prevButton = document.querySelector('[data-js="button-prev"]');
-// const nextButton = document.querySelector('[data-js="button-next"]');
-const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
 let maxPage = 42;
@@ -31,6 +28,20 @@ const nextButton = createNavButton("next", () => {
   fetchCharacter()
 })
 
+const pagination = createPagination();
+
+const searchBar = createSearchBar((event) => {
+  event.preventDefault();
+  searchQuery = event.target.elements.query.value;
+  page = 1;
+  fetchCharacter()
+});
+
+navigation.append(prevButton, pagination,nextButton);
+searchBarContainer.append(searchBar);
+
+fetchCharacter()
+
 async function fetchCharacter(){
   const result = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}&name=${searchQuery}`
   );
@@ -42,4 +53,3 @@ cardContainer.innerHTML = ""
 characters.map(createCard).forEach((card)=> cardContainer.append(card))
 console.log("data:",data);
 };
-fetchCharacter()
