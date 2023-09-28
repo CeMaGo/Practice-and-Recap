94c95ad37bf7f54ed26f690653c3401386f90400
CeMaGo <119850565+CeMaGo@users.noreply.github.com>
2023-09-28 20:33:18 +0200

    usestate practice 2

---
+++ React-State/react-state_counter/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+const [count, setCount] = useState(0);
+
+function incrementCount()
+{ 
+  return setCount(count + 1)
+}
+function decrementCount() {
+  return setCount(count - 1)
+}
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <div className="button-container">
+        <button
+          type="button"
+          onClick={decrementCount }
+        >
+          -
+        </button>
+        <button
+          type="button"
+          onClick={ incrementCount }
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
