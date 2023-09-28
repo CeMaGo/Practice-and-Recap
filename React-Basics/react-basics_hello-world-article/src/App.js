198a50f3ec28299a10b2b92b5226bad6af53da53
CeMaGo <119850565+CeMaGo@users.noreply.github.com>
2023-09-28 18:26:17 +0200

    Basics React

---
+++ React-Basics/react-basics_hello-world-article/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return HelloWorldArticle();
+}
+
+function HelloWorldArticle() {
+return( 
+<a>
+<h1>Hello World!</h1>
+<p>This is a paragraph...</p>
+</a>  
+);
+}
