f51128eab8aafedb49aa21042327bb41b616dc02
CeMaGo <119850565+CeMaGo@users.noreply.github.com>
2023-10-04 12:10:44 +0200

    Button build done

--- React-Props/react-props_button/src/App.js
+++ React-Props/react-props_button/src/App.js
@@ -2,20 +2,22 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
+  function handleClick() {return  alert('You clicked the Button!')}
+
   return <Button 
           color="blue" 
           text="click me" 
           disabled={!true}
+          onClick={handleClick}
         ></Button>;
 }
 
 
-function Button({ color, disabled, text }){
- function handleClick() {return  alert('You clicked the Button!')}
-
+// blueprint, think of it as a blueprint or a struct, rather general building part, functionality can be assigned later when actually i  use
+function Button({ color, disabled, text, onClick}){
   return (
     <button 
-            onClick={handleClick}
+            onClick={onClick}
             style={{backgroundColor:color,
                     padding:'10px 20px' ,
                     border: 'none',
