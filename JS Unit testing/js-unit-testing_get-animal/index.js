9d9b2509ef8d4f76f4873ed14d351f43b20ee41f
CeMaGo <119850565+CeMaGo@users.noreply.github.com>
2023-11-13 19:04:04 +0100

    import error

---
+++ JS Unit testing/js-unit-testing_get-animal/index.js
@@ -0,0 +1,5 @@
+export default function getAnimal(argument) {
+if (argument === "cats") return ("I totally love cats!");
+else if (argument === "animalPluralName") return ("I like dogs!");
+else if (!argument) return ("I do not like animals at all!");
+}
